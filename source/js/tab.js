a5ab1a6f052a2450677f28f9733fa7fdd024d144
Mariya <alisa-m2@yandex.ru>
2019-03-17 14:16:26 +0300

    New

---
+++ source/js/tab.js
@@ -0,0 +1,22 @@
+$(function(){
+$('.tab-panels>div').hide();
+$('.tab-nav a').click(function(){                         // При клике на вкладку
+$('.tab-panels>div').hide().filter(this.hash).show();   // - показывается блок с описанием текущей вкладки
+$('.tab-nav li').removeClass('select');                 // - удаляется класс 'select' у активной ранее вкладки
+$(this).parent().addClass('select');                    // - добавляется класс 'select' для выбранной вкладки
+return (false);                                         // - прерывается обработка события onClick
+})
+$(function(){
+$ ('.pop-block').each(function(){
+var closeTrigger = $(this).find('.close-block').length;
+if (closeTrigger < 1 ){ // проверяем наличие кнопки и если нет - добавляем
+$(this).append('<span class="close-block"></span>');
+}else{
+return '';
+}
+$('.close-block').on('click', function(){
+$(this).closest('.pop-block').fadeOut(100);
+});
+});
+});
+})
